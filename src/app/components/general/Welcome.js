ad18f36065f3ead655b4cfef1f029943237175bb
DavidJoao <davidsandoval596@gmail.com>
2025-01-27 17:19:05 -0800

    ADDED Motion animations

--- src/app/components/general/Welcome.js
+++ src/app/components/general/Welcome.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { Dancing_Script } from "next/font/google"
 import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 import Link from 'next/link';
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 import { useAppContext } from '../context';
 
 const dancingScript = Dancing_Script({
@@ -14,30 +14,36 @@ const dancingScript = Dancing_Script({
 
 const Welcome = () => {
 
-    const { setIsShowing } = useAppContext()
+    const { setIsShowing, isShowing } = useAppContext()
 
   return (
-    <div className='w-screen h-screen bg-black flex flex-col items-center justify-center gap-2 p-5'>
-        <motion.div initial={{ scale: 0 }} animate={{ scale: 1}}>
-            <Image height={200} width={200} src={"/pic.jpeg"} alt='pic' className='rounded-full'/>
-        </motion.div>
-        <h1 className={`${dancingScript.variable} name text-3xl font-extrabold -rotate-[5deg] m-[30px] text-center`}>- David Joao Sandoval -</h1>
-        <p className='text-center'>Software Developer / Full Stack Developer</p>
-        <button className='black-button' onClick={() => setIsShowing(false)}>Find Out More</button>
-        <div className='flex flex-row gap-3'>
-            <motion.button initial={{ scale: 0 }} animate={{ scale: 1 }}>
-                <Link href={"https://www.linkedin.com/in/davidjoaosandoval/"} target='_blank'> <FaLinkedin className='social-icon'/> </Link>
-            </motion.button>
+    <AnimatePresence mode='wait'>
+        {isShowing &&
+        <motion.div key="box" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}>
+        <div className='w-screen h-screen bg-black flex flex-col items-center justify-center gap-2 p-5'>
+            <motion.div initial={{ scale: 0 }} animate={{ scale: 1}}>
+                <Image height={200} width={200} src={"/pic.jpeg"} alt='pic' className='rounded-full border border-white'/>
+            </motion.div>
+            <h1 className={`${dancingScript.variable} name text-3xl font-extrabold -rotate-[5deg] m-[30px] text-center`}>- David Joao Sandoval -</h1>
+            <p className='text-center'>Software Developer / Full Stack Developer</p>
+            <button className='black-button' onClick={() => setIsShowing(false)}>Find Out More</button>
+            <div className='flex flex-row gap-3'>
+                <motion.button initial={{ scale: 0 }} animate={{ scale: 1 }}>
+                    <Link href={"https://www.linkedin.com/in/davidjoaosandoval/"} target='_blank'> <FaLinkedin className='social-icon'/> </Link>
+                </motion.button>
 
-            <motion.button initial={{ scale: 0 }} animate={{ scale: 1}}>
-                <Link href={"https://github.com/DavidJoao/"} target='_blank'> <FaGithub className='social-icon'/> </Link>
-            </motion.button>
+                <motion.button initial={{ scale: 0 }} animate={{ scale: 1}}>
+                    <Link href={"https://github.com/DavidJoao/"} target='_blank'> <FaGithub className='social-icon'/> </Link>
+                </motion.button>
 
-            <motion.button initial={{ scale: 0 }} animate={{ scale: 1 }}>
-                <Link href={"https://www.instagram.com/david.jso/"} target='_blank'> <FaInstagram className='social-icon'/> </Link>
-            </motion.button>
+                <motion.button initial={{ scale: 0 }} animate={{ scale: 1 }}>
+                    <Link href={"https://www.instagram.com/david.jso/"} target='_blank'> <FaInstagram className='social-icon'/> </Link>
+                </motion.button>
+            </div>
         </div>
-    </div>
+        </motion.div> 
+        }
+    </AnimatePresence>
   )
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,10 @@
 'use client'
-import Image from "next/image";
-import { useContext, useState } from "react";
 import Welcome from "./components/general/Welcome";
-import { useAppContext } from "./components/context";
 
 export default function Home() {
-
-  const { isShowing, setIsShowing } = useAppContext()
-
-  if (isShowing) return <Welcome />
-
   return (
     <div>
-      
+      <Welcome />
     </div>
   );
 }
