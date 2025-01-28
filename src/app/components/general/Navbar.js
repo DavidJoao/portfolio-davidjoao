3b9a21b7ff49d6ef8307c7cd9e64b610d21d1a17
DavidJoao <davidsandoval596@gmail.com>
2025-01-27 21:11:55 -0800

    ADDED Tracking of selected tab

--- src/app/components/general/Navbar.js
+++ src/app/components/general/Navbar.js
@@ -1,20 +1,28 @@
 'use client'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import { useAppContext } from '../context'
 
 const Navbar = () => {
 
-  const { isShowing } = useAppContext()
+  const { isShowing, currentTab, setCurrentTab } = useAppContext()
 
   if (isShowing) return <></>
+  
   return (
-    <div className='border p-2 flex flex-row-reverse gap-2'>
-        <Link href={'/'}></Link>
-        <Link href={'/'}>Contact</Link>
-        <Link href={'/'}>Projects</Link>
-        <Link href={'/'}>About</Link>
-        <Link href={'/'}>Home</Link>
+    <div className='p-3 md:pr-10 flex justify-center md:justify-start flex-row-reverse gap-5 border text-xs md:text-base fixed w-full bg-black'>
+        <Link href={'/contact'} onClick={(e) => {
+          setCurrentTab(e.target.innerHTML)
+          }} className={currentTab === "Contact" ? "nav-link selected-nav-link" : "nav-link text-white"}>Contact</Link>
+        <Link href={'/projects'} onClick={(e) => {
+          setCurrentTab(e.target.innerHTML)
+          }} className={currentTab === "Projects" ? "nav-link selected-nav-link" : "nav-link text-white"}>Projects</Link>
+        <Link href={'/about'} onClick={(e) => {
+          setCurrentTab(e.target.innerHTML)
+          }} className={currentTab === "About" ? "nav-link selected-nav-link" : "nav-link text-white"}>About</Link>
+        <Link href={'/'} onClick={(e) => {
+          setCurrentTab(e.target.innerHTML)
+          }} className={currentTab === "Home" ? "nav-link selected-nav-link" : "nav-link text-white"}>Home</Link>
     </div>
   )
 }
