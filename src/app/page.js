77fc74802ef7c581eec54481602271c989f72421
DavidJoao <davidsandoval596@gmail.com>
2025-01-27 21:12:57 -0800

    CREATED: Main Layout for homepage

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,14 @@ import Welcome from "./components/general/Welcome";
 
 export default function Home() {
   return (
-    <div>
+    <div className="border w-screen h-screen flex flex-col md:flex-row items-center justify-center">
       <Welcome />
+      <div className="border h-1/3 md:h-screen w-screen md:w-1/3">
+
+      </div>
+      <div className="border h-2/3 md:h-screen w-screen md:w-2/3">
+
+      </div>
     </div>
   );
 }
