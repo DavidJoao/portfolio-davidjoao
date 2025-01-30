b8484d88349ea96e2a0ad7402ccff3dda77c7b59
DavidJoao <davidsandoval596@gmail.com>
2025-01-29 21:40:55 -0800

    FIX Bug

--- src/app/page.js
+++ src/app/page.js
@@ -4,15 +4,20 @@ import { FaGit, FaAws } from "react-icons/fa";
 import { IoLogoPython, IoLogoJavascript, IoLogoNodejs, IoLogoReact, IoLogoCss3, IoLogoHtml5, IoLogoVercel } from "react-icons/io5";
 import { SiNextdotjs, SiPostgresql, SiMongodb } from "react-icons/si";
 import { IoIosArrowDropdown } from "react-icons/io";
+import { useAppContext } from "./components/context";
 
 export default function Home() {
+
+    const { isShowing } = useAppContext()
+
+    if (isShowing) return <Welcome />
+
 	return (
 		<div className="w-screen h-screen flex flex-col md:flex-row items-center justify-center">
-			<Welcome />
+			
 			<div className="h-auto md:h-screen w-screen md:w-1/2 flex flex-col items-center md:items-end justify-center p-5">
 				<div className="flex flex-col items-center justify-center gap-3">
 					<img src="/memoji.png" className="w-[250px] h-[250px] sm:h-auto sm:w-auto" />
-					<button className="black-button">Download Resume</button>
 				</div>
 			</div>
 			<div className="h-2/3 md:h-screen w-screen md:w-1/2 flex flex-col items-center md:items-start justify-start md:justify-center text-center md:pt-[50px] md:p-2">
