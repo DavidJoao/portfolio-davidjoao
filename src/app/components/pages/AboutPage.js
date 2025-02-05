a19cae6eeb70d6e496f668b6a526d42d2b8a9e88
DavidJoao <davidsandoval596@gmail.com>
2025-02-04 19:41:45 -0800

    CHANGE About page

--- src/app/components/pages/AboutPage.js
+++ src/app/components/pages/AboutPage.js
@@ -5,39 +5,54 @@ const AboutPage = () => {
 	const { setPage, setCurrentTab } = useAppContext()
 
 	return (
-		<div className="md:pt-[50px] h-auto min-h-screen w-screen flex flex-col-reverse md:flex-row bg-black">
-			<div className="h-full w-full md:w-1/2 p-5 bg-black flex flex-col items-end text-center gap-2">
-				<div>
-					<h1 className="font-black text-[30px] mb-5">So, who am I?</h1>
-					<p className="max-w-[600px]">
-						I&#39;m a Software Engineer specializing in web development, leveraging my
-						skills to build and deploy full-stack web applications. I work with
-						technologies like JavaScript, React, Next.js, Node.js, Python, PostgreSQL,
-						MongoDB, MySQL, Tailwind CSS, React Bootstrap, and Amazon Web Services to
-						create efficient, scalable, and user-friendly solutions.
-					</p>
-					<p className="max-w-[600px] mt-4">
-						My goal is to grow professionally while delivering high-quality, optimized
-						web applications that meet and exceed expectations.
-					</p>
-				</div>
-				<div className="h-auto max-w-[600px] mt-5 bg-neutral-700 p-4 rounded text-center">
-                    <h1 className="mb-5 text-[30px] font-black">Outside Work</h1>
-					<p>
-						In my free time, I love exploring new restaurants as a true foodie and I&#39;m always
-						on the hunt for great coffee. I also enjoy traveling, and going to concerts to immerse myself in
-						live music. In fact, my favorite band is The Killers, and nothing beats the
-						energy of seeing them perform live!
-					</p>
-                    <p> If you haven&#39;t heard them yet, give them a try with my personal recommendation—you won&#39;t be disappointed </p>
-                    <iframe className="mt-4 w-[300px] h-[200px] mx-auto" width="560" height="315" src="https://www.youtube.com/embed/zPJQQeeYDYI?si=ms5zIOmjFSc5E4qR" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                    
-				</div>
+		<div className="h-auto md:h-[calc(100vh-4rem)] p-2 w-screen grid grid-cols-1 md:grid-cols-2 grid-rows-2">
+
+			<div className="flex flex-col md:items-start md:justify-center p-4 md:p-[35px] gap-2">
+				<h1 className="font-black text-[30px]">So, who am I?</h1>
+				<p className="max-w-[600px]">
+					I&#39;m a Software Engineer specializing in web development, leveraging my
+					skills to build and deploy full-stack web applications. I work with technologies
+					like JavaScript, React, Next.js, Node.js, Python, PostgreSQL, MongoDB, MySQL,
+					Tailwind CSS, React Bootstrap, and Amazon Web Services to create efficient,
+					scalable, and user-friendly solutions.
+				</p>
+				<p className="max-w-[600px] mt-4">
+					My goal is to grow professionally while delivering high-quality, optimized web
+					applications that meet and exceed expectations.
+				</p>
 			</div>
 
-			<div className="h-auto md:h-full w-full md:w-1/2 p-5 flex justify-center items-start md:items-center bg-black">
-				<img src="pcmemoji.png" className="w-[350px] md:w-2/3" />
+			<div className="flex items-start justify-center md:items-center md:justify-center md:p-[35px]">
+				<img src="pcmemoji.png" className="w-[350px]" />
 			</div>
+
+			<div className="flex items-start justify-center p-4 md:p-[35px]">
+				<iframe
+					className="w-full h-full rounded-xl border border-neutral-500"
+					width="560"
+					height="315"
+					src="https://www.youtube.com/embed/zPJQQeeYDYI?si=ms5zIOmjFSc5E4qR"
+					title="YouTube video player"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+					referrerPolicy="strict-origin-when-cross-origin"
+					allowFullScreen></iframe>
+			</div>
+
+			<div className="flex flex-col items-start md:items-end justify-start p-[35px] gap-2 text-left md:text-right">
+				<h1 className="font-black text-[30px]">Outside Work</h1>
+				<p>
+					In my free time, I love exploring new restaurants as a true foodie and I&#39;m
+					always on the hunt for great coffee. I also enjoy traveling, and going to
+					concerts to immerse myself in live music. In fact, my favorite band is The
+					Killers, and nothing beats the energy of seeing them perform live!
+				</p>
+				<p>
+					{" "}
+					If you haven&#39;t heard them yet, give them a try with my personal
+					recommendation—you won&#39;t be disappointed{" "}
+				</p>
+			</div>
+
 		</div>
 	)
 }
