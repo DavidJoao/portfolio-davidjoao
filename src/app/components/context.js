b91ef168a4554d374bc6c25fb190afba00c65432
DavidJoao <davidsandoval596@gmail.com>
2025-02-08 15:44:53 -0800

    FIX Text

--- src/app/components/context.js
+++ src/app/components/context.js
@@ -4,37 +4,20 @@ import { createContext, useContext, useEffect, useCallback, useState } from "rea
 const AppContext = createContext()
 
 const Provider = ({ children }) => {
-	const [notification, setNotification] = useState("")
-	const [theme, setTheme] = useState("light")
 
 	useEffect(() => {
 		const savedTheme = localStorage.getItem("theme")
 		if (savedTheme) {
 			document.documentElement.classList.toggle("dark", savedTheme === "dark")
-			setTheme(savedTheme)
 		} else {
 			const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches
 			const initialTheme = prefersDarkScheme ? "dark" : "light"
 			document.documentElement.classList.toggle("dark", initialTheme === "dark")
-			setTheme(initialTheme)
 		}
 	}, [])
 
-	const toggleTheme = () => {
-		const newTheme = theme === "dark" ? "light" : "dark"
-		document.documentElement.classList.toggle("dark", newTheme === "dark")
-		setTheme(newTheme)
-		localStorage.setItem("theme", newTheme)
-	}
-
 	return (
-		<AppContext.Provider
-			value={{
-				notification,
-				setNotification,
-				toggleTheme,
-				theme,
-			}}>
+		<AppContext.Provider value={{}}>
 			{children}
 		</AppContext.Provider>
 	)
